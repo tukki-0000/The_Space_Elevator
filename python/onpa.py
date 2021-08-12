12dce81b9c3b89d450afcd807227cdc6f915b9c8
tukki-0000 <tukki_0711@icloud.com>
2021-08-12 12:08:36 +0900

    update

---
+++ python/onpa.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+TRIG_PIN = 14
+ECHO_PIN = 15
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(TRIG_PIN,GPIO.OUT)
+GPIO.setup(ECHO_PIN,GPIO.IN)
+GPIO.setwarnings(False)
+
+def pulseIn(PIN, start=1, end=0):
+    if start==0: end = 1
+    t_start = 0
+    t_end = 0
+
+    while GPIO.input(PIN) == end:
+        t_start = time.time()
+        
+    while GPIO.input(PIN) == start:
+        t_end = time.time()
+    return t_end - t_start
+
+while True:
+    GPIO.output(TRIG_PIN, GPIO.LOW)
+    time.sleep(0.01)
+  
+    GPIO.output(TRIG_PIN, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG_PIN, False)
+
+    t = pulseIn(ECHO_PIN)
+
+    distance = 34000 * t/2
+    print(distance, "cm")
